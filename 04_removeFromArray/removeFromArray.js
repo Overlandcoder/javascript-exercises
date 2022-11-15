12bd14170e73d7e1a12e3a06b2e44466aab94f71
Overlander <anuragnandal@hotmail.com>
2022-11-14 20:22:08 -0500

    Finish exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...args) {
+  for (const element of args) {
+    if (array.includes(element)) {
+      i = array.indexOf(element);
+      array.splice(i, 1);
+    }
+  };
+  return array;
 };
 
 // Do not edit below this line
