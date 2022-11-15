56afb74da29dbc96d77249eda702850cf19865f9
Overlander <anuragnandal@hotmail.com>
2022-11-15 15:40:20 -0500

    Finish

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(n1, n2) {
+	return n1 + n2;
 };
 
-const subtract = function() {
-	
+const subtract = function(n1, n2) {
+	return n1 - n2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let sum = 0;
+	for (const value of array) {
+    sum += value;
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let product = 1;
+  for (const value of array) {
+    product *= value;
+  }
+  return product;
 };
 
-const power = function() {
-	
+const power = function(n1, n2) {
+  return n1 ** n2;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let factorial = 1;
+  for (i = 1; i <= num; i++) {
+    factorial *= i;
+  }
+  return factorial;
 };
 
 // Do not edit below this line
