b6e59de014e1fa891edea90ace33a4640dd72d92
Overlander <anuragnandal@hotmail.com>
2022-11-17 21:19:04 -0500

    Finish

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+  if (num < 0) return "OOPS";
+  
+  let fibs = [1, 1];
 
+  for (i = 1; i <= num; i++) {
+    let newnum = fibs.at(-1) + fibs.at(-2);
+    fibs.push(newnum);
+  }
+  return fibs[num - 1];
 };
 
 // Do not edit below this line
