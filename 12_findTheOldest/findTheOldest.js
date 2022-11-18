d9eb4f937cb9b4bb0a94a05b8314319570ba0fee
Overlander <anuragnandal@hotmail.com>
2022-11-18 12:06:36 -0500

    Finish

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+  oldest = 0;
+  let oldestPerson;
+  people.forEach((person) => {
+    age = person.yearOfDeath - person.yearOfBirth;
+    if (oldest < age) {
+      oldest = age;
+      oldestPerson = person;
+    }
+  })
+  return oldestPerson;
 };
 
 // Do not edit below this line
